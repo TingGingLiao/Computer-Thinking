ca2182950cdca8f22425d5fa2614da8e29925ef6
TingGingLiao <71249739+TingGingLiao@users.noreply.github.com>
2020-12-28 13:59:34 +0800

    Add files via upload

---
+++ 廖挺均Week16.py
@@ -0,0 +1,34 @@
+print(type(True))
+print(type(False))
+
+print(bool(1))
+print(bool(0))
+print(bool())
+
+print(False+3)
+score=0
+score=66
+print(score==66)
+
+name="Francis"
+print(name=="Francis")
+print(name=="francis")
+
+print(True & True)
+print(True & False)
+print(False & True)
+print(False & False)
+print(True and False)
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+print(not True)
+print(not False)
+x=70
+print((x>60))and((x<80))
+print((x>60))or((x<80))
+print(not(x>60))
+
+"H" in "Hello world"
+"h" in "Hello world"
